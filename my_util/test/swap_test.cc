14389c3b215789ee8dd73d1bd0a3ce3135d8c3b3
Remo Dietlicher <remo.d@gmx.ch>
2023-01-05 13:03:28 +0100

    add array test for swap

--- my_util/test/swap_test.cc
+++ my_util/test/swap_test.cc
@@ -5,11 +5,20 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(MyUtilTest, TestIntegers) {
+TEST(SwapTest, TestIntegers) {
   int a = 5;
   int b = 8;
 
   my_util::swap(&a, &b);
   EXPECT_EQ(a, 8);
   EXPECT_EQ(b, 5);
+}
+
+TEST(SwapTest, TestArrayElement) {
+  int a[2] = {5, 8};
+
+
+  my_util::swap(&a[0], &a[1]);
+  EXPECT_EQ(a[0], 8);
+  EXPECT_EQ(a[1], 5);
 }
